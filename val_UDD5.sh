3672be4e1fcb22430628681dbf109249f29325d8
QT-Zhu <zhuqingtian971114@126.com>
2021-01-27 13:55:26 +0800

    Delete val_UDD5.sh

--- val_UDD5.sh
+++
@@ -1,8 +0,0 @@
-python3 train.py \
-    --cuda True \
-    --resume checkpoints/ENet_UDD5_epoch300.pth.tar \
-    --dataset UDD5 \
-    --data_path ~/datasets/UDD5 \
-    --model ENet \
-    --init_eval False \
-    --mode val
